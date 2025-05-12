2eb832b70a12f3de29bdbafb41e6167ce6c6e993
Makar <166397206+macarcls@users.noreply.github.com>
2025-05-12 17:48:35 +0300

    Добавьте файлы проекта.

---
+++ VDK/Image_zoom.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace VDK
+{
+    /// <summary>
+    /// Логика взаимодействия для Image_zoom.xaml
+    /// </summary>
+    public partial class Image_zoom : Window
+    {
+        public Image_zoom(BitmapImage imageb)
+        {
+            InitializeComponent();
+            image.Source = imageb;
+        }
+
+        // Обработчик закрытия окна
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            this.Owner.IsEnabled = true; // Размораживаю MainWindow
+        }
+    }
+}
---
+++ VDK/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using LiveCharts.Wpf;
+using LiveCharts;
+using System.Drawing;
+using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System;
+using System.Runtime.Serialization;
+
+namespace VDK;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    Resourses _viewModel;
+    public MainWindow()
+    {
+        InitializeComponent();
+     
+        _viewModel = new Resourses();
+        DataContext = _viewModel;
+        
+    }
+
+    private void Upload_photo_Click(object sender, RoutedEventArgs e)
+    {
+        // Open dialog
+        var dialog = new Microsoft.Win32.OpenFileDialog();
+        dialog.DefaultExt = ".png";
+        bool? result = dialog.ShowDialog();
+
+
+        // Process open file dialog box results
+        if (result == true)
+        {
+            // Записываю путь к файлу в filename и создаю битмап
+            _viewModel.filename = dialog.FileName;
+            BitmapImage imageBitm = new BitmapImage(new Uri(_viewModel.filename, UriKind.Absolute));
+
+            // Обрезаю круговое изображение
+            var circ_crop = new CroppedBitmap(imageBitm, new Int32Rect(512, 512, 200, 200));
+            var imageBrush = new ImageBrush
+            {
+                ImageSource = circ_crop,
+                Stretch = Stretch.None
+            };
+
+            
+            cropped.Source = new CroppedBitmap(imageBitm, new Int32Rect(0, 1024, 512, 120)); // Информация по фотографии
+            image_p.Source = new CroppedBitmap(imageBitm, new Int32Rect(0, 0, 1024, 1024)); // Полное изображение
+            ellipse.Fill = imageBrush;
+            Zoom_im.IsEnabled = true;
+            Zoom_im.Visibility = Visibility.Visible; 
+        }
+    }
+    
+    // Обработчик нажатия по изображению (открывает окно Image_zoom)
+    private void ZoomPhoto(object sender, RoutedEventArgs e)
+    {
+
+        BitmapImage imageBitm = new BitmapImage(new Uri(_viewModel.filename!, UriKind.Absolute));
+        Image_zoom zoom = new Image_zoom(imageBitm);
+        zoom.Owner = this; // Делаю связь для обращения из Image_zoom к MainWindow
+        zoom.Show();
+        mainw.IsEnabled = false; // Замораживаю окно
+    }
+}
